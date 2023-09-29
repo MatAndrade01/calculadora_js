64e06555a7dd1c0fc2983947dd2c9b0b0734ca79
Matheus Andrade <ltbmatheus141012@gmail.com>
2023-09-29 16:15:14 -0300

    Pressione as teclas para calcular

--- atividade_04.10/calculadora/calculadora.js
+++ atividade_04.10/calculadora/calculadora.js
@@ -1,67 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(){
-    
-    const somar = document.querySelector(".botaoAdicao");
-    const subtrair = document.querySelector(".botaoSubtracao");
-    const dividir = document.querySelector(".botaoDivisao");
-    const multiplicar = document.querySelector(".botaoMutiplicacao");
+function insert (num) {
 
-    somar.addEventListener("click", function(e){
-        e.preventDefault();
+   var numero =  document.getElementById("resultado").innerHTML;
+   document.getElementById("resultado").innerHTML =  numero + num;
+}
 
-        const v1 = parseFloat(document.querySelector(".valor01").value);
-        const v2 = parseFloat(document.querySelector(".valor02").value);
+function clean(){
+    document.getElementById('resultado').innerHTML = "";
+}
 
-        const soma = (v1 + v2);
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
 
-        document.querySelector(".valor").innerHTML = soma;
-
-        document.querySelector(".valor01").value = "";
-        document.querySelector(".valor02").value = "";
-
-    })
-
-    subtrair.addEventListener("click", function(e){
-        e.preventDefault();
-
-        const v1 = parseFloat(document.querySelector(".valor01").value);
-        const v2 = parseFloat(document.querySelector(".valor02").value);
-        
-
-        const subitracao = (v1 - v2);
-
-        document.querySelector(".valor").innerHTML = subitracao;
-
-        document.querySelector(".valor01").value = "";
-        document.querySelector(".valor02").value = "";
-        
-    })
-
-    dividir.addEventListener("click", function(e){
-        e.preventDefault();
-
-        const v1 = parseFloat(document.querySelector(".valor01").value);
-        const v2 = parseFloat(document.querySelector(".valor02").value);
-
-        const divisao = (v1 / v2);
-
-        document.querySelector(".valor").innerHTML = divisao;
-
-        document.querySelector(".valor01").value = "";
-        document.querySelector(".valor02").value = "";
-        
-    })
-
-    multiplicar.addEventListener("click", function(e){
-        e.preventDefault();
-
-        const v1 = parseFloat(document.querySelector(".valor01").value);
-        const v2 = parseFloat(document.querySelector(".valor02").value);
-
-        const multiplicacao = (v1 * v2);
-
-        document.querySelector(".valor").innerHTML = multiplicacao
-
-        document.querySelector(".valor01").value = "";
-        document.querySelector(".valor02").value = "";
-    })
-});
+    if (resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    }
+}
